30f5ecdc0ca825ebefc97979f12b748a124dc49a
Arjun <atupe96@gmail.com>
2022-11-06 21:05:15 +0530

    Mixed

--- Client.java
+++ Client.java
@@ -1,4 +1,11 @@
 Hii 
+<<<<<<< Updated upstream
 HR 
 your mention Project amount is out of my budest
 please in amoun with in 6lak
+=======
+Dear HR ..
+Date 3/11/2203
+Project: bus Booking System
+project complet date=12/4/2024
+>>>>>>> Stashed changes
--- HR.java
+++ HR.java
@@ -1,3 +1,10 @@
+<<<<<<< Updated upstream
 Project amount is 879900lac
 project complte eith in onemonth;
 thanks you contact for TCS
+=======
+Hii 
+Dear Cleint yes 
+your reurement is complet for your metion date 
+thank you 
+>>>>>>> Stashed changes
