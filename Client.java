cf746956e98a1b2747d5a4799b368c1e618064ec
Arjun <atupe96@gmail.com>
2022-11-06 21:02:25 +0530

    update

---
+++ Client.java
@@ -0,0 +1,4 @@
+Hii 
+HR 
+your mention Project amount is out of my budest
+please in amoun with in 6lak
---
+++ HR.java
@@ -0,0 +1,3 @@
+Project amount is 879900lac
+project complte eith in onemonth;
+thanks you contact for TCS
