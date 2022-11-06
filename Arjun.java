f8b88d082cc36468d059496d3c4a2c7fa2c9bcd4
Arjun <atupe96@gmail.com>
2022-11-06 20:22:21 +0530

    CodeAdded

--- Arjun.java
+++ Arjun.java
@@ -1,6 +1,14 @@
+<<<<<<< Updated upstream
 Java Tutorial
 Java Training
 Our core Java programming tutorial is designed for students and
  working professionals. Java is an object-oriented, class-based, concurrent, 
 secured and general-
 purpose computer-programming language. It is a widely used robust technology.
+=======
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
+>>>>>>> Stashed changes
