bb5bd226fb69917201934360e793d348a63d4fdb
Arjun <atupe96@gmail.com>
2022-11-06 19:53:03 +0530

    Mixed

--- Practice.java
+++ Practice.java
@@ -1,7 +1,17 @@
+<<<<<<< Updated upstream
 public void mehtod(){
 
 public static void (stringc [args] ){
 
 system.out.println(methde);
 
+=======
+public class Demo{
+
+public satic void main (String [args] )
+int a=10;
+int b=12;
+c=a+b;
+System.out.println(c);
+>>>>>>> Stashed changes
 }
