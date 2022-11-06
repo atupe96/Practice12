4f0544e0ba0876078827baa15740e5c5e426ec4a
Arjun <atupe96@gmail.com>
2022-11-06 20:49:45 +0530

    createCode

--- Employee.java
+++ Employee.java
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 public class Companies{
 
 public static void main(String [args] ){
 System.out.pritln("Companies");
+=======
+public class Employee{
+
+public static void main (String [args]){
+
+System.out.println(Employee);
+>>>>>>> Stashed changes
 }
